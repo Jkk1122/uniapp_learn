b5880dba40c27794fcacfe8cdcbde53d7d475b67
JiaXinke <12975557+jstarstar@user.noreply.gitee.com>
2023-08-10 23:22:05 +0800

    demo1day1

---
+++ demo1/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="box">
+		<h1>uniappj基础<span>入门</span></h1>
+	</div>
+</template>
+
+<script>
+	export default {
+	}
+</script>
+
+<style lang="scss">
+	.box{
+		background-color: pink;
+		width: 100%;
+		height: 300px;
+		h1{
+			font-size: 30px;
+			color: brown;
+				
+			span{
+				font-weight: 500;
+			}
+		}
+	}
+</style>
